5063fcb6b33828f83c83299e4fc05ee8a196c616
G-mirna <140032921+G-mirna@users.noreply.github.com>
2024-01-20 15:26:32 -0600

    Add files via upload

---
+++ ACTIVIDAD1/TAREA 1/Ejercicio4.js
@@ -0,0 +1,7 @@
+let arreglo = [10, 11, 3, 20, 5, 10];
+
+if(arreglo[0]===arreglo[5]){
+    console.log("El primer y utlimo elemento del areeglo son iguales" , arreglo[0] + " true "+arreglo[5]);
+}else{
+    console.log("El primer y utlimo elemento del areeglo  no son iguales" ,arreglo[0] +" False "+arreglo[5]);
+}
